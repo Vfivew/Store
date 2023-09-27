fd4e276bf7c612b673d17ff8362a93eba69007c1
Vfivew <hitori555111555@yahoo.com>
2023-09-27 18:54:44 +0300

    Main/Banner/Button

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,7 +5,11 @@ const Main = () => {
         <main className="main">
             <section className="grid-container">
                 <section className="block1">
-                    <Slider/>
+                    <Slider />
+                    <div className='block-sticker'>
+                        <h3>Великий вибір вудилищ на котушок</h3>
+                        <p>До каталогу</p>
+                    </div>
                 </section>
                 <section className="block2"></section>
                 <section className="block3"></section>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,13 +2,19 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
 import Main from "../components/Main/Main";
+import BannerDiscount from "../components/Banner/BannerDiscount/BannerDiscount";
+import BannerInfo from "../components/Banner/BannerInfo/BannerInfo";
+import Button from "../components/Button/Button";
 
 const HomePage = () => {
     return (
         <>
             <Header />
             <Navbar />
-            <Main/>
+            <Button/>
+            <Main />
+            <BannerInfo/>
+            <BannerDiscount/>
             <Footer/>
         </>
     );
